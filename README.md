30876d923a07eab19f6dcac3fdf0bb00732694d6
qiuxinping <qiuxinping@bytedance.com>
2024-10-12 12:58:46 +0800

    .

--- README.md
+++ README.md
@@ -1,13 +1,14 @@
+# 这是什么？
+这是飞书多维表格仪表盘插件-html开发模板。
+关于仪表盘插件，您可以在[多维表格仪表盘插件开发指南](https://bytedance.larkoffice.com/docx/X0DRdskrpoxf8kxfFj1csLtNnmc)中了解更多信息 ）。
+
+
 # 开始
-- 点击绿色run按钮
+- `npm i` 安装依赖包。
+- `npm run dev` 启动项目
 - 编辑 [index.ts](#src/index.ts) 并观看实时更新！
-
-# 了解更多
-
-您可以在[多维表格扩展脚本开发指南](https://feishu.feishu.cn/docx/U3wodO5eqome3uxFAC3cl0qanIe)中了解更多信息 ）。
-
-## 安装包
-在 Shell 窗格中安装npm包或在 Packages 窗格中搜索并添加。
+- 编辑完成后，`npm run build` 打包项目。
+- 然后可以选择上架飞书多维表格仪表盘插件市场，给所有人使用。或者使用您自己的服务器部署，供您公司内部使用。
 
 ## 国际化
 本模板使用[jquery-i18next](https://locize.com/blog/jquery-i18next/)进行国际化。
@@ -38,39 +39,38 @@ data-i18n-options用于插值，同$.t函数的第二个参数，将替换语言
 
 
 
+# What is this?
+This is the Feishu Multi-dimensional Table Dashboard Plugin-html development template.
+For dashboard plugins, you can learn more in the [Multi-dimensional Table Dashboard Plugin Development Guide](https://bytedance.larkoffice.com/docx/X0DRdskrpoxf8kxfFj1csLtNnmc) ).
 
-# Getting Started
-- Hit run
-- Edit [index.ts](#src/index.ts) and watch it live update!
+# Get Started
+- `npm i` installs dependency packages.
+- `npm run dev` starts the project
+- Edit [index.ts](#src/index.ts) and watch the real-time updates!
+- After editing, `npm run build` packages the project.
+- Then you can choose to list it on the Feishu Multi-dimensional Table Dashboard Plugin Market for everyone to use. Or use your own server deployment for internal use in your company.
 
-# Learn More
-
-You can learn more in the [Base Extension Development Guide](https://lark-technologies.larksuite.com/docx/HvCbdSzXNowzMmxWgXsuB2Ngs7d)
-
-## Install packages
-
-Install packages in Shell pane or search and add in Packages pane.
-
-
-## globalization
+## Internationalization
 This template uses [jquery-i18next](https://locize.com/blog/jquery-i18next/) for internationalization.
-- Called through $.t() in the js file, such as in Chinese environment:
+- Called in js file by $.t(), such as in Chinese environment:
 ```js
-console.log($.t('content', {num:888})) // '这是中文内容888'
-console.log($.t('title')) // '这是中文标题'
+console.log($.t('content', {num:888})) // 'This is Chinese content 888'
+console.log($.t('title')) // 'This is Chinese title'
 ```
 
-- Use in tags:
-By setting the attribute data-i18n to the key configured in a certain language, when using that language, the value corresponding to the key will be used to overwrite the content of the tag, thereby achieving internationalization.
-data-i18n-options are used for interpolation. They are the same as the second parameter of the $.t function and will replace the variables wrapped in {{}} in the language configuration.
+- Used in tags:
+By setting the attribute data-i18n to a key in a language configuration, when using the language, the value corresponding to the key will be used to overwrite the content of the tag, thereby achieving internationalization.
+data-i18n-options is used for interpolation, the same as the second parameter of the $.t function, which will replace the variables wrapped by {{}} in the language configuration.
+
 ```html
-<h1 data-i18n="title">默认标题</h1>
+<h1 data-i18n="title">Default title</h1>
 
 <h2 data-i18n="content" data-i18n-options='{"num":888}'> </h2>
 ```
 
-If you want to use it in an element without child elements such as input, you need to prefix the data-i18n attribute value with [the label attribute you want to assign].
-For example, configure internationalization for the placeholder attribute of input:
+If you want to use it in an element without child elements such as input, you need to add the prefix [the tag attribute you want to assign] to the data-i18n attribute value.
+For example, configure the internationalization of the placeholder attribute of input:
+
 ```html
 <input data-i18n="[placeholder]title"/>
 
--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,139 @@
 import $ from 'jquery';
-import { bitable } from '@lark-base-open/js-sdk';
+import { bitable, DashboardState, Rollup } from '@lark-base-open/js-sdk';
 import './index.scss';
 // import './locales/i18n'; // 开启国际化，详情请看README.md
 
 
-$(async function() {
-  const [tableList, selection] = await Promise.all([bitable.base.getTableMetaList(), bitable.base.getSelection()]);
+$(async function () {
+  const status = bitable.dashboard.state;
+
+  if (status === DashboardState.Config || status === DashboardState.Create) {
+    //配置状态.
+    config();
+    return;
+  }
+  if (status === DashboardState.View) {
+    //仪表盘展示状态
+    view();
+    return;
+  }
+
+  if (status === DashboardState.FullScreen) {
+    //仪表盘展示全屏状态
+    view();
+    return;
+  }
+});
+
+// 配置状态下相关逻辑
+async function config() {
+  $('#config_block').show();
+  const tableList = await bitable.base.getTableMetaList();
   const optionsHtml = tableList.map(table => {
     return `<option value="${table.id}">${table.name}</option>`;
   }).join('');
-  $('#tableSelect').append(optionsHtml).val(selection.tableId!);
-  $('#addRecord').on('click', async function() {
-    const tableId = $('#tableSelect').val();
-    if (tableId) {
-      const table = await bitable.base.getTableById(tableId as string);
-      table.addRecord({
-        fields: {},
-      });
+  $('#tableSelect').append(optionsHtml).val('');
+
+  /** 从表单获取DataConditions数据源范围和配置。给saveConfig使用，控制getData获取到的数据 */
+  const getDataConditions = () => {
+    const tableId: string | undefined = $('#tableSelect').val() as any;
+    const groups: string | undefined = $('#tableSelect_groups').val() as any;
+    const series: string | undefined = $('#tableSelect_series').val() as any;
+
+    if (!tableId || !groups || !series) {
+      return;
     }
+    return [{
+      tableId,
+      series: [{// 系列
+        fieldId: series,
+        rollup: Rollup.SUM
+      }],
+      groups: [{// 分组信息
+        fieldId: groups,
+      }]
+    }]
+  }
+
+  // 展示预览状态的数据
+  const showPreviewData = async () => {
+    const dataConditions = getDataConditions();
+    if (!dataConditions) {
+      return;
+    }
+
+    const previewData = await bitable.dashboard.getPreviewData(dataConditions);
+    console.log('====preivewData', previewData); // 配置状态下可以获取到预览数据。
+    showData(previewData);
+  }
+
+  // 处理配置时候的预览逻辑
+  $('#tableSelect').on('change', async function () {
+    console.log('===on change')
+    const tableId: string | undefined = $('#tableSelect').val() as any;
+    if (!tableId) {
+      return;
+    }
+    const table = await bitable.base.getTableById(tableId);
+    const fieldMetaList = await table.getFieldMetaList();
+
+    const fieldOptionsHtml = fieldMetaList.map(f => {
+      return `<option value="${f.id}">${f.name}</option>`;
+    }).join('');
+    // 设置字段选择器的选项
+    $('#tableSelect_groups').html(fieldOptionsHtml).val('');
+    $('#tableSelect_series').html(fieldOptionsHtml).val('');
   });
-});
+
+  $('#config_form').on('change', async function () {
+    showPreviewData();
+  })
+
+  // 预览完成后保存配置
+  $('#save_btn').on('click', async function () {
+    const dataConditions = getDataConditions();
+
+    if (!dataConditions) {
+      alert('请选择数据源');
+      return;
+    }
+    // 保存配置.
+    await bitable.dashboard.saveConfig({
+      dataConditions,
+      customConfig: {
+        aa: Math.random(),//保存一些自定义配置项
+      }
+    })
+  });
+}
+
+async function view() {
+  $('#view_block').show();
+  // 上一次通过saveConfig保存的数据
+  const config = await bitable.dashboard.getConfig();
+  const showDataOnChange = async () => {
+    const data = await bitable.dashboard.getData();//拿到dataConditions数据源的数据
+    console.log('===config,data', {
+      config,
+      data,
+    });
+    // 一些数据处理逻辑...
+    showData(data);
+  }
+  showDataOnChange();
+
+  bitable.dashboard.onConfigChange(async () => {
+    console.log('===view 配置改变')
+    showDataOnChange();
+  })
+
+  bitable.dashboard.onDataChange(async () => {
+    showDataOnChange();
+  })
+}
+
+function showData(data: object) {
+
+  $('#view_block_data').text(JSON.stringify(data, null, '  '));
+
+}
